b54e7486d16a0cb04e97ef5000505d31d32d8acd
ao.lv@vloveapp.com <lvao>
2017-09-21 15:02:32 +0800

    日历和历史页面修改

--- GoodThingsHappenEverDay/main/code/Calender.js
+++ GoodThingsHappenEverDay/main/code/Calender.js
@@ -6,48 +6,98 @@ import {
   ScrollView,
   Image,
 } from 'react-native';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Calendar, CalendarList, Agenda,LocaleConfig } from 'react-native-calendars';
+import { NavigationActions, } from 'react-navigation';
+
+import { Level,Calender1} from './common/constants_titel';
 var Dimensions = require('Dimensions');
 var width = Dimensions.get('window').width;
 var height = Dimensions.get('window').height;
-import { getItem, saveItem} from './common/AsyncStorage'
+LocaleConfig.locales['fr'] = {
+  monthNames: ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
+ 
+  dayNamesShort: ['周日','周一','周二','周三','周四','周五','周六']
+};
+
+LocaleConfig.defaultLocale = 'fr';
+import { getItem, saveItem} from './common/AsyncStorage';
  class Calender extends Component {
      constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+          minday:'',
+          today:'',
+          selected:'',
+          level1:1,
+
+
+        };
         this.onDayPress = this.onDayPress.bind(this);
       }
   componentWillMount() {
     var promise = saveItem("one", '3', () => { }).then((result) => {
+          let newDate = new Date();
+          let newDay = newDate.toJSON();
+          let thisday = newDay.slice(0,10);
+          let days = -3;
+          let newMinDate = new Date();
+          newMinDate.setDate(newMinDate.getDate()+days); 
+          let mDay = newMinDate.toJSON();
+          let minday = mDay.slice(0,10);
+          // console.log('====================================');
+          // console.log(newMinDate);
+          // console.log(minday)
+          // console.log('====================================');
+          this.setState({today:thisday,minday:minday})
+            var promise = getItem("level").then((result) => {
+              
+              this.setState({level1:parseInt(result)})
+            }).catch((error) => {
+              console.error(new Error("失败"));
+            })
           // alert('1')
         }).catch((error) => {
           console.error(new Error("失败"));
         })
   }
+  componentDidMount() {
+  //   var promise = getItem("level").then((result) => {
+  //   LocaleConfig.locales['fr'] = {
+  //     monthNames: ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
+  //     monthNamesShort: ['Janv.','Févr.','Mars','Avril','Mai','Juin','Juil.','Août','Sept.','Oct.','Nov.','Déc.'],
+  //     dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+  //     dayNamesShort: ['周日','周一','周二','周三','周四','周五','周六']
+  //   };
+    
+  //   LocaleConfig.defaultLocale = 'fr';
+  // }).catch((error) => {
+  //   console.error(new Error("失败"));
+  // })
+    
+  }
 
   render() {
     return (
       <ScrollView style={styles.container}>
-            <View style={{flexDirection:'row',backgroundColor:'#fff',borderColor:'#666',borderBottomWidth:1}}>
+            <View style={{flexDirection:'row',backgroundColor:'#4FA4FF',}}>
                 <View style={styles.topView}>
-                      <Text style={styles.topText}>Calendar</Text>
+                      <Text style={styles.topText}>{Calender1}</Text>
                       
                 </View>
-                <Image style={{height:40,width:40,alignItems:'center',justifyContent:'center'}} 
-                      source={require('./image/hot.png')}
-                      ><Text style={{textAlign:'center',fontWeight:'bold',fontSize:20}}
-                >1</Text></Image>
+                <View style={{height:50,width:40,alignItems:'center',justifyContent:'center',flexDirection:'row'}} 
+                      
+                      ><Image source={require('./image/level.png')} style={{height:12,width:15,marginRight:2}}/><Text style={{textAlign:'center',fontSize:12,color:'#F6FCFF'}}
+                >{Level}{this.state.level1}</Text></View>
             </View>
-        <Text style={styles.text}>Calendar with selectable date and arrows</Text>
+        
         <Calendar
-        // maxDate={'2017-08-8'}
-          onDayPress={this.onDayPress}
-          style={styles.calendar}
-          // hideExtraDays
-          // hideExtraDays={true}
-          markedDates={{[this.state.selected]: {selected: true}}}
-           
-        />
+        minDate={this.state.minday}
+        maxDate={this.state.today}
+        onDayPress={this.onDayPress}
+        style={styles.calendar}
+        hideExtraDays
+        
+      />
        
        
       </ScrollView>
@@ -55,9 +105,26 @@ import { getItem, saveItem} from './common/AsyncStorage'
     );
   }
   onDayPress(day) {
-    this.setState({
-      selected: day.dateString
-    });
+    let tday = day.timestamp;
+    var promise = saveItem("Calenderday", day.dateString, () => { }).then((result) => {
+      var promise = saveItem("texinput1", '0', () => { }).then((result) => {
+        var promise = saveItem("texinput2", '0', () => { }).then((result) => {
+        var promise = saveItem("Calender", tday.toString(), () => { }).then((result) => {
+          // console.log(day.timestamp-28800000+1)
+          const { navigate } = this.props.navigation;
+          navigate('Roots');
+        }).catch((error) => {
+          console.error(new Error("失败"));
+        })
+      }).catch((error) => {
+        console.error(new Error("失败"));
+      })
+    }).catch((error) => {
+      console.error(new Error("失败"));
+    })
+    }).catch((error) => {
+        console.error(new Error("失败"));
+    })
   }
 }
 
@@ -99,13 +166,13 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     alignItems:'center',
     flexDirection:'row',
-    backgroundColor:'#fff'
+    backgroundColor:'#4FA4FF'
   },
   topText:{
     textAlign:'center',
     fontSize:20,
     fontWeight:'bold',
-    color:'#000',
+    color:'#F6FCFF',
     paddingLeft:20,
   },
 });
--- GoodThingsHappenEverDay/main/code/History.js
+++ GoodThingsHappenEverDay/main/code/History.js
@@ -5,41 +5,253 @@ import {
   View,
   ScrollView,
   Image,
+  FlatList,
 } from 'react-native';
+import { getItem,saveItem} from './common/AsyncStorage'
  import ScrollableTabView, {DefaultTabBar,ScrollableTabBar} from 'react-native-scrollable-tab-view';
+ import { History1,Level,Week,Months,Year,All} from './common/constants_titel';
 var Dimensions = require('Dimensions');
 var width = Dimensions.get('window').width;
 var height = Dimensions.get('window').height;
 var ScreenWidth = Dimensions.get('window').width;
 
  class History extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+       
+        keyarry:[],
+        WeekArry:[],
+        MonthArry:[],
+        YearArry:[],
+        AllArry:[],
+        level1:1,
+      
+    };
+    
+  }
+  componentWillMount() {
+    
+    let timestamp = Date.parse(new Date());
+    let thistime = ''+timestamp+''
+    let Yeartime = thistime-31536000000
+    let Monthtime = thistime-2592000000
+    let Weektime = thistime-604800000
+    var promise = getItem("keyarry1").then((result) => {
+      // alert(JSON.parse(result)) 
+      this.setState({keyarry:JSON.parse(result)})
+      // alert(this.state.keyarry)
+      let keyarry = this.state.keyarry; 
+      for(let i in this.state.keyarry){
+        var promise = getItem(keyarry[i]).then((result) => {
+          this.state.AllArry.push(result)
+            // alert('12')
+        }).catch((error) => {
+          
+        })
+        if(keyarry[i]>Weektime&&keyarry[i]<timestamp){
+          var promise = getItem(keyarry[i]).then((result) => {
+            this.state.WeekArry.push(result)
+              
+          }).catch((error) => {
+            
+          })
+            // this.state.WeekArry.push()
+            // alert(this.state.WeekArry)
+          // alert(keyarry[1])
+        }
+        if(keyarry[i]>Monthtime&&keyarry[i]<timestamp){
+          var promise = getItem(keyarry[i]).then((result) => {
+            this.state.MonthArry.push(result)
+              
+          }).catch((error) => {
+            
+          })
+        }
+        if(keyarry[i]>Yeartime&&keyarry[i]<timestamp){
+          var promise = getItem(keyarry[i]).then((result) => {
+            this.state.YearArry.push(result)
+              
+          }).catch((error) => {
+            
+          })
+        }
+      }
+      
+      
+    }).catch((error) => {
+    // console.log('1');
 
+
+    })
+    // this.setState({WeekArry:WeekArry})
+  }
+  componentDidMount() {
+    var promise = getItem("level").then((result) => {
+      // alert(parseInt(result))
+      this.setState({level1:parseInt(result)})
+    }).catch((error) => {
+      console.error(new Error("失败"));
+    })
+  }
+  _separator = () => {
+    return <View style={{height:2,}}/>;
+  }
+  _renderItemWeek = (item) => {
+    let journltext = item.item.slice(11)
+    let thtime = item.item.slice(0,7)
+    let day =  item.item.slice(8,10)
+    let date = item.item.slice(0,4)+'.'+item.item.slice(5,7)
+    return ( 
+              <View style={styles.allCard}>
+                <View style={{flex:2,justifyContent:'flex-start',alignItems:'center',}}>
+                  <Text style={{fontSize:18,color:'#F2E0A9',lineHeight:18}}>{day}</Text>
+                </View>
+                <View style={{flex:15,}}>
+                  <View style={{flexDirection:'row',height:15,}}>
+                    <Image source={require('./image/history-icon.png')} style={{height:10,width:10}}></Image>
+                    <Text style={{fontSize:10,lineHeight:10,color:'#b3b3b3'}}>{date}</Text>
+                  </View> 
+                  
+                  <Text style={{fontSize:14,color:'#666',}}>{journltext}</Text>
+                </View>
+                
+               
+              </View>
+      )
+  }
+  _renderItemMonth = (item) => {
+    let journltext = item.item.slice(11)
+    let thtime = item.item.slice(0,7)
+    let day =  item.item.slice(8,10)
+    let date = item.item.slice(0,4)+'.'+item.item.slice(5,7)
+    return ( 
+              <View style={styles.allCard}>
+                <View style={{flex:2,justifyContent:'flex-start',alignItems:'center',}}>
+                  <Text style={{fontSize:18,color:'#F2E0A9',lineHeight:18}}>{day}</Text>
+                </View>
+                <View style={{flex:15,}}>
+                  <View style={{flexDirection:'row',height:15,}}>
+                    <Image source={require('./image/history-icon.png')} style={{height:10,width:10}}></Image>
+                    <Text style={{fontSize:10,lineHeight:10,color:'#b3b3b3'}}>{date}</Text>
+                  </View> 
+                  
+                  <Text style={{fontSize:14,color:'#666',}}>{journltext}</Text>
+                </View>
+                
+               
+              </View>
+      )
+  }
+  _renderItemYear = (item) => {
+    let journltext = item.item.slice(11)
+    let thtime = item.item.slice(0,7)
+    let day =  item.item.slice(8,10)
+    let date = item.item.slice(0,4)+'.'+item.item.slice(5,7)
+    return ( 
+              <View style={styles.allCard}>
+                <View style={{flex:2,justifyContent:'flex-start',alignItems:'center',}}>
+                  <Text style={{fontSize:18,color:'#F2E0A9',lineHeight:18}}>{day}</Text>
+                </View>
+                <View style={{flex:15,}}>
+                  <View style={{flexDirection:'row',height:15,}}>
+                    <Image source={require('./image/history-icon.png')} style={{height:10,width:10}}></Image>
+                    <Text style={{fontSize:10,lineHeight:10,color:'#b3b3b3'}}>{date}</Text>
+                  </View> 
+                  
+                  <Text style={{fontSize:14,color:'#666',}}>{journltext}</Text>
+                </View>
+                
+               
+              </View>
+      )
+  }
+  _renderItemAll = (item) => {
+    let journltext = item.item.slice(11)
+    let thtime = item.item.slice(0,7)
+    let day =  item.item.slice(8,10)
+    let date = item.item.slice(0,4)+'.'+item.item.slice(5,7)
+    return ( 
+              <View style={styles.allCard}>
+                <View style={{flex:2,justifyContent:'flex-start',alignItems:'center',}}>
+                  <Text style={{fontSize:18,color:'#F2E0A9',lineHeight:18}}>{day}</Text>
+                </View>
+                <View style={{flex:15,}}>
+                  <View style={{flexDirection:'row',height:15,}}>
+                    <Image source={require('./image/history-icon.png')} style={{height:10,width:10}}></Image>
+                    <Text style={{fontSize:10,lineHeight:10,color:'#b3b3b3'}}>{date}</Text>
+                  </View> 
+                  
+                  <Text style={{fontSize:14,color:'#666',}}>{journltext}</Text>
+                </View>
+                
+               
+              </View>
+      )
+  }
   render() {
     return (
-      <ScrollView>
-            <View style={{flexDirection:'row',backgroundColor:'#fff',borderColor:'#666',borderBottomWidth:1}}>
-                <View style={styles.topView}>
-                      <Text style={styles.topText}>History</Text>
-                      
+            <View style={{flex:1,backgroundColor: '#F5FCFF',marginTop:20}}>
+                <View style={{flexDirection:'row',backgroundColor:'#4FA4FF',}}>
+                    <View style={styles.topView}>
+                          <Text style={styles.topText}>{History1}</Text>
+                          
+                    </View>
+                    <View style={{height:50,width:40,alignItems:'center',justifyContent:'center',flexDirection:'row'}} 
+                          
+                          ><Image source={require('./image/level.png')} style={{height:11,width:15,marginRight:2}}/><Text style={{textAlign:'center',fontSize:12,color:'#F6FCFF'}}
+                    >{Level}{this.state.level1}</Text></View>
                 </View>
-                <Image style={{height:40,width:40,alignItems:'center',justifyContent:'center'}} 
-                      source={require('./image/hot.png')}
-                      ><Text style={{textAlign:'center',fontWeight:'bold',fontSize:20}}
-                >1</Text></Image>
-            </View>
             <ScrollableTabView
-                  style={styles.container}
+                  style={{marginTop: 0, }}
                   renderTabBar={() => <DefaultTabBar />}
                   tabBarUnderlineStyle={styles.lineStyle}
-                  tabBarActiveTextColor='#FF0000'>
+                  tabBarInactiveTextColor='#666'
 
-                  <Text style={styles.textStyle} tabLabel='Past Week'>No Entries</Text>
-                  <Text style={styles.textStyle} tabLabel='Past Month'>No Entries</Text>
-                  <Text style={styles.textStyle} tabLabel='Past Year'>No Entries</Text>
-                  <Text style={styles.textStyle} tabLabel='All Time'>No Entries</Text>
+                  tabBarActiveTextColor='#4FA4FF'
+                
+                  >
+                  
+                
+                  <ScrollView tabLabel={Week} style={styles.tabView}>
+                      <FlatList
+                    
+                      ItemSeparatorComponent={this._separator}
+                      renderItem={this._renderItemWeek}
+                      data={this.state.WeekArry}>
+                      </FlatList>
+                      <View style={styles.bottomView}></View>
+                </ScrollView>
+                <ScrollView tabLabel={Months} style={styles.tabView}>
+                    <FlatList
+                    
+                      ItemSeparatorComponent={this._separator}
+                      renderItem={this._renderItemMonth}
+                      data={this.state.MonthArry}>
+                      </FlatList>
+                      <View style={styles.bottomView}></View>
+                </ScrollView>
+                <ScrollView tabLabel={Year} style={styles.tabView}>
+                      <FlatList
+                      
+                        ItemSeparatorComponent={this._separator}
+                        renderItem={this._renderItemYear}
+                        data={this.state.YearArry}>
+                        </FlatList>
+                        <View style={styles.bottomView}></View>
+              </ScrollView>
+              <ScrollView tabLabel={All} style={styles.tabView}>
+                      <FlatList
+                      
+                        ItemSeparatorComponent={this._separator}
+                        renderItem={this._renderItemAll}
+                        data={this.state.AllArry}>
+                        </FlatList>
+                        <View style={styles.bottomView}></View>
+              </ScrollView>
+                  
             </ScrollableTabView>
-      </ScrollView>
+            </View>
     );
   }
 }
@@ -69,7 +281,7 @@ const styles = StyleSheet.create({
      lineStyle: {
          width:ScreenWidth/4,
          height: 2,
-         backgroundColor: '#FF0000',
+         backgroundColor: '#4FA4FF',
      },
      textStyle: {
          flex: 1,
@@ -83,14 +295,44 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     alignItems:'center',
     flexDirection:'row',
-    backgroundColor:'#fff'
+    backgroundColor:'#4FA4FF'
   },
   topText:{
     textAlign:'center',
     fontSize:20,
     fontWeight:'bold',
-    color:'#000',
+    color:'#F6FCFF',
     paddingLeft:20,
   },
+  tabView: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'rgba(0,0,0,0.01)',
+  },
+  card: {
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    borderColor: 'rgba(0,0,0,0.1)',
+    margin: 5,
+    height: 100,
+    padding: 15,
+    shadowColor: '#ccc',
+    shadowOffset: { width: 2, height: 2, },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    borderRadius:10,
+  },
+  allCard:{
+    height: 130,
+    margin: 5,
+    // height: 100,
+    padding: 15,
+    borderRadius:10,
+    elevation: 2,
+    flexDirection:'row'
+  },
+  bottomView:{
+    height:20,
+  }
 });
 export default History
